4c0f1f1cf183f4a394f7740983d56292011a04e1
Javier Gongora <jgongora@gmail.com>
2022-05-26 15:31:19 -0700

    Add merge-sort function using recursion

--- specs/merge-sort/merge-sort.test.js
+++ specs/merge-sort/merge-sort.test.js
@@ -7,12 +7,44 @@
 */
 
 const mergeSort = (nums) => {
-  // code goes here
+  // base case, return array if length 1 or 0
+  if (nums.length < 2) {
+    return nums;
+  }
+
+  // break into two smaller arrays
+  const length = nums.length;
+  const middle = Math.floor(length / 2);
+  const left = nums.slice(0, middle);
+  const right = nums.slice(middle);
+
+  // call mergeSort on left (array1) and right (array2)
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  // return the merge of left (array1) and right (array2)
+  return merge(sortedLeft, sortedRight);
+};
+
+// create merge function
+const merge = (left, right) => {
+  const results = [];
+
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+
+  // return one sorted array
+  return results.concat(left, right);
 };
 
 // unit tests
 // do not modify the below code
-test.skip("merge sort", function () {
+test("merge sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const ans = mergeSort(nums);
   expect(ans).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
