dbd4e3c40b3ab308560fcd2fce26ab24a7fb387d
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-05-25 22:35:52 -0700

    initial commit

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -10,28 +10,11 @@
   index's. Continue looping through until all values are in ascending order
 */
 
-function bubbleSort(nums) {
-  let swaps = false;
-  let length = nums.length;
-  do {
-    swaps = false;
-    for (let i = 1; i <= length; i++) {
-      if (nums[i - 1] > nums[i]) {
-        let a = nums[i - 1];
-        let b = nums[i];
-        nums[i - 1] = b;
-        nums[i] = a;
-        swaps = true;
-      }
-    }
-  } while (swaps);
-
-  return nums;
-}
+function bubbleSort(nums) {}
 
 // unit tests
 // do not modify the below code
-test("bubble sort", function () {
+test.skip("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
