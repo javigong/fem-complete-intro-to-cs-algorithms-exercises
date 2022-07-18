e1a32b482c94b83568c33b4aee05a95e2c78b1bc
Javier Gongora <jgongora@gmail.com>
2022-07-18 15:29:39 -0700

    Add linkedList by using nodes

--- specs/arrays/linkedlist.test.js
+++ specs/arrays/linkedlist.test.js
@@ -30,19 +30,50 @@ class LinkedList {
     this.length = 0;
   }
   push(value) {
-    
+    const node = new Node(value);
+    this.length++;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      this.tail.next = node;
+    }
+    this.tail = node;
   }
   pop() {
-    
+    return this.delete(this.length - 1);
   }
   _find(index) {
-    
+    if (index >= this.length) return null;
+    let current = this.head;
+    for (let i = 0; i <= index - 1; i++) {
+      current = current.next;
+    }
+
+    return current;
   }
   get(index) {
-    
+    const node = this._find(index);
+    if (!node) return void 0;
+    return node.value;
   }
   delete(index) {
-    
+    if (index === 0) {
+      const head = this.head;
+      if (head) {
+        this.head = head.next;
+      } else {
+        this.head = null;
+      }
+      this.length--;
+      return head.value;
+    }
+
+    const node = this._find(index - 1);
+    const excise = node.next;
+    node.next = excise.next;
+    if (!node.next) this.tail = node.next;
+    this.length--;
+    return excise.value;
   }
 }
 
