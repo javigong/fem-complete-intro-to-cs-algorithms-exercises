767738f588f0b79ea68393e3f564366083863bb3
Javier Gongora <jgongora@gmail.com>
2022-07-19 21:48:16 -0700

    Add binary search tree to tree visualizer

--- src/Tree.js
+++ src/Tree.js
@@ -4,23 +4,57 @@ import { TreeViz } from "./tree-visualizer";
 import _ from "lodash";
 
 class Tree {
-  add(num) {
-    // needs this
+  // code goes here
+  constructor() {
+    this.root = null;
+  }
+  add(value) {
+    // some logic around if this is the root
+    if (this.root === null) {
+      this.root = new Node(value);
+    } else {
+      let current = this.root;
+      while (true) {
+        if (current.value > value) {
+          // go left
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(value);
+            break;
+          }
+        } else {
+          // go right
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(value);
+            break;
+          }
+        }
+      }
+    }
+    return this;
   }
   toObject() {
-    // returns a root node with
-    // {
-    //   value:  <number>
-    //   left:   <object>
-    //   right:  <object>
-    //   height: <number, optional>
-    // }
-    return { value: 1, left: null, right: null, height: 1 };
+    return this.root;
+  }
+}
+
+// you might consider using a Node class too
+class Node {
+  // code maybe goes here
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
 export default function TreeComponent() {
-  const nums = _.shuffle(_.range(50));
+  // const nums = _.shuffle(_.range(10));
+  // adding 
+  const nums = _.range(100);
   const tree = new Tree();
   nums.map((num) => tree.add(num));
   const objs = tree.toObject();
